d773a7527651f77e8f25ae379c846d8266010d32
haems82 <student@administrators-iMac-272.local>
2016-02-04 13:41:53 -0800

    Setup of Standard Page
    Basic HTML, CSS, JS.

---
+++ website/js/app.js
@@ -0,0 +1,23 @@
+
+
+/* Use the Module Pattern to create our app */
+
+var Wedding = (function () {
+
+	// deafault app Id.
+	var id = 0;
+
+	/**
+	* @method init
+	* @description initialize the application's JS
+	*/
+	function init () {
+
+	};
+
+	return {
+		init: init
+
+	}; //end of returned object
+
+})(); //immediate execution
